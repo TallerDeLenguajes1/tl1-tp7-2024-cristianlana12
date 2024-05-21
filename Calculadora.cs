5b51d77dafa4da81f5deb74a1042d8333be739d4
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-05-21 12:41:23 -0300

    Finalizacion del ejercicio01

---
+++ Calculadora.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
+        private double dato;
+        public double Resultado { get => dato; }
+
+        public void Sumar(double num)
+        {
+            this.dato = dato + num;
+        }
+
+        public void Restar(double num)
+        {
+            this.dato = dato - num;
+        }
+
+        public void Multiplicar(double num)
+        {
+            this.dato = dato * num;
+        }
+
+        public void Dividir(double num)
+        {
+            this.dato = dato / num;
+        }
+
+        public void Limpiar()
+        {
+            this.dato = 0;
+        }
+    }
+
+}
